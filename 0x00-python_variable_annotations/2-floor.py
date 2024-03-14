6ab7b22bef234b8853379383f5923ba6208e9388
ekarkat <ekarkat@gmail.com>
2024-03-14 16:25:08 +0000

    task 3

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -3,5 +3,6 @@
 
 
 def floor(n: float) -> int:
+    """ return the floor of a float"""
     n_int = int(n)
     return n_int
--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" Floor of a float"""
+
+
+def floor(n: float) -> str:
+    """ return a string representation of a float"""
+    n_str = str(n)
+    return n_str
