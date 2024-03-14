f789529e553008f482745908b972b60f626f72e0
ekarkat <ekarkat@gmail.com>
2024-03-14 16:19:20 +0000

    task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""concat  function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ type-annotated function """
+    sum_str = str1 + str2
+    return sum_str
