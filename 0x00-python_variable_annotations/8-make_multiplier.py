9a418859028bc54abb7bcf0ca15870e283b013fe
ekarkat <ekarkat@gmail.com>
2024-03-14 17:05:04 +0000

    task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Write a type-annotated function sum_list
+which takes a list input_list of floats as argument
+and returns their sum as a float.
+
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    multiplier : float
+    Callable : list float
+    return : Float
+    """
+
+    def foo(num: float):
+        return num * multiplier
+
+    return foo
