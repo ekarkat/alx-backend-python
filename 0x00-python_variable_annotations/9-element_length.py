4619ae5d314a434f47353bbb7df16ba3315dfdaf
ekarkat <ekarkat@gmail.com>
2024-03-14 17:14:21 +0000

    task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Write a type-annotated function sum_list
+which takes a list input_list of floats as argument
+and returns their sum as a float.
+
+"""
+from typing import Sequence, Iterable, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    lis : Sequence
+    Callable : list float
+    return : Float
+    """
+    return [(i, len(i)) for i in lst]
