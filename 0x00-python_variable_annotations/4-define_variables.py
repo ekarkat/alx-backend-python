bf5f43b6c6a8dde260fc4e4645fc51d76a39e999
ekarkat <ekarkat@gmail.com>
2024-03-14 16:47:30 +0000

    task 4

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-a, an integer with a value of 1
-pi, a float with a value of 3.14
-i_understand_annotations, a boolean with a value of True
-school, a string with a value of “Holberton”
+Define and annotate the following variables with the specified values:
+
 """
 
 
--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Write a type-annotated function floor
+which takes a float n as argument
+and returns the floor of the float.
+
+"""
+
+
+def sum_list(n: float) -> str:
+    """
+    n : float
+    return : str
+    """
+    return str(n)
