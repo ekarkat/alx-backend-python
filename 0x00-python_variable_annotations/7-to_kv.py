eb432e815e49cdc6966832bcbfab933713989e9e
ekarkat <ekarkat@gmail.com>
2024-03-14 17:02:37 +0000

    task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Write a type-annotated function sum_list
+which takes a list input_list of floats as argument
+and returns their sum as a float.
+
+"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    k: str
+    v: list
+    return : tupple
+    """
+    return (k, v**2)
