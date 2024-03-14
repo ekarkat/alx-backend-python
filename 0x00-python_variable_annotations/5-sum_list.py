e4e7fd3fc649ae25602ca74c0fda324ce939cceb
ekarkat <ekarkat@gmail.com>
2024-03-14 16:56:33 +0000

    edit task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -8,12 +8,12 @@ and returns their sum as a float.
 from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     input_list : float
     return : float
     """
-    sum_input = 0
+    sum_input: float = 0
     for n in input_list:
         sum_input = sum_input + n
     return sum_input
