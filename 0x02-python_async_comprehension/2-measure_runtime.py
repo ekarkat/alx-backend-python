a1f630602e2e58110bff0570e04a206427219212
ekarkat <ekarkat@gmail.com>
2024-03-19 21:47:35 +0000

    task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Module Doc """
+import asyncio
+import time
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Documentation"""
+    
+    lis = [async_comprehension() for i in range(4)]
+    start = time.perf_counter()
+    res = await asyncio.gather(*lis)
+    finish = time.perf_counter()
+    return finish - start
