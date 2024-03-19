a41e2c45ef0673e11fa8974d6ceef518592924b6
ekarkat <ekarkat@gmail.com>
2024-03-19 21:25:23 +0000

    task 0 1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Documentation"""
     for i in range(10):
         await asyncio.sleep(1)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,10 +5,10 @@ import random
 from typing import List
 
 
-async_generator = __import__('0-async_generator').async_generator
+async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_generator() -> List[float]:
+async def async_comprehension() -> List[float]:
     """Documentation"""
     result = []
     async for i in async_generator():
