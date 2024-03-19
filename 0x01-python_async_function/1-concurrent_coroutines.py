1f4e3f93188eed403437c3db815a72a3213d0c15
ekarkat <ekarkat@gmail.com>
2024-03-19 00:24:29 +0000

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+""" Modul doc """
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """function doc"""
+    tasks = []
+    for i in range(n):
+        tasks.append(asyncio.create_task(wait_random(max_delay)))
+
+    results = await asyncio.gather(*tasks)
+    return sorted(results)
