9decfc80d1d52c390ce8c941ed78305d37666fb9
ekarkat <ekarkat@gmail.com>
2024-03-19 00:40:34 +0000

    rasl 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+""" Modul doc """
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """function doc"""
+    tasks = []
+    for i in range(n):
+        tasks.append(task_wait_random(max_delay))
+
+    results = await asyncio.gather(*tasks)
+    return sorted(results)
