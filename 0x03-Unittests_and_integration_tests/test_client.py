d1e0845b08737de118ac6892c88a800a70c68653
ekarkat <ekarkat@gmail.com>
2024-04-09 04:00:38 +0100

    tak 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,34 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Test client unitest"""
+import unittest
+from typing import Dict
+from unittest.mock import (
+    MagicMock,
+    Mock,
+    patch,
+)
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test GithubOrgClient"""
+
+    @parameterized.expand([
+        ("google", {'test': "google"}),
+        ("abc", {'test': "abc"}),
+    ])
+    @patch(
+        "client.get_json",
+    )
+    def test_org(self, org: str, expected_response: Dict,
+                 mock_fn: MagicMock) -> None:
+        """test client.get_json"""
+
+        mock_fn.return_value = expected_response
+        client = GithubOrgClient(org_name)
+        org_response = client.org()
+        self.assertEqual(org_response, expected_response)
+        mock_fn.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org)
+        )
